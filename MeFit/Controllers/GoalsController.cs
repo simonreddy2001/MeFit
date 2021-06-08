e4c8fcbae440dc20553a81bc812b956957a921a9
simon reddy kommareddy <77497059+simonreddy2001@users.noreply.github.com>
2021-06-08 16:00:49 +0200

    controlers

---
+++ MeFit/Controllers/GoalsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MeFit.Models;
+
+namespace MeFit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalsController : ControllerBase
+    {
+        private readonly MeFitDBContext _context;
+
+        public GoalsController(MeFitDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Goals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Goal>>> GetGoals()
+        {
+            return await _context.Goals.ToListAsync();
+        }
+
+        // GET: api/Goals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Goal>> GetGoal(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            return goal;
+        }
+
+        // PUT: api/Goals/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGoal(int id, Goal goal)
+        {
+            if (id != goal.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(goal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GoalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Goals
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Goal>> PostGoal(Goal goal)
+        {
+            _context.Goals.Add(goal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGoal", new { id = goal.Id }, goal);
+        }
+
+        // DELETE: api/Goals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGoal(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Goals.Remove(goal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GoalExists(int id)
+        {
+            return _context.Goals.Any(e => e.Id == id);
+        }
+    }
+}
